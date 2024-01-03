35b0587d9b7070a93fbd5baac1f22520b224a56e
hp <hp@UZAIR>
2024-01-03 15:01:53 +0500

    Add project files.

---
+++ Controllers/StudentResultsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StudentResultsController : ControllerBase
+{
+    private static List<StudentResult> _studentResults = new List<StudentResult>
+    {
+        new StudentResult { Id = 1, StudentName = "Uzair Ali", Score = 90 },
+        new StudentResult { Id = 2, StudentName = "Inaam Ullah", Score = 85 }
+   
+    };
+
+    [HttpGet]
+    public ActionResult<IEnumerable<StudentResult>> Get()
+    {
+        return Ok(_studentResults);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<StudentResult> Get(int id)
+    {
+        var result = _studentResults.FirstOrDefault(r => r.Id == id);
+        if (result == null)
+            return NotFound();
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public ActionResult<StudentResult> Post(StudentResult studentResult)
+    {
+        studentResult.Id = _studentResults.Count + 1;
+        _studentResults.Add(studentResult);
+        return CreatedAtAction(nameof(Get), new { id = studentResult.Id }, studentResult);
+    }
+
+}
---
+++ Models/StudentResult.cs
@@ -0,0 +1,8 @@
+﻿public class StudentResult
+{
+    public int Id { get; set; }
+    public string StudentName { get; set; }
+    public int Score { get; set; }
+    
+
+}
